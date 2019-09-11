5b247feb207b20cc51d7616377ddd2e7f707cb45
Mona Lisa <mona.lisa@github.com>
2019-09-11 10:34:46 +0300

    working on covering Departament with unit tests

--- DepartamentTests/DepartamentTests.cs
+++ DepartamentTests/DepartamentTests.cs
@@ -13,19 +13,29 @@ namespace Tests
         }
 
         [Test]
-        public void EmployeeIsNotNull()
+        [TestCase(18)]
+        [TestCase(52)]
+        [TestCase(60)]
+        public void Validate_Age_Positive(int actual)
         {
-            //arrange
-
             //act
-            if (employee != null)
-            {
-                //assert
-                Assert.IsNotNull(employee, "Employee is not null");
-            }
-
-
-
+            if (actual >= 18 || actual <= 60)
+            //assert
+            Assert.IsTrue(true, "Age is valid");
         }
+
+
+        //public void EmployeeIsNotNull()
+        //{
+        //    //arrange
+
+        //    //act
+        //    if (employee != null)
+        //    {
+        //        //assert
+        //        Assert.IsNotNull(employee, "Employee is not null");
+        //    }
+
+        //}
     }
 }
